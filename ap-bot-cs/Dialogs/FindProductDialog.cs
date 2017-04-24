55ac615f3cd68556180549e7bdcb435324cb8869
manisbindra <maninder.bindra@gmail.com>
2017-04-24 15:20:45 +0530

    initial commit

---
+++ ap-bot-cs/Dialogs/FindProductDialog.cs
@@ -0,0 +1,26 @@
+﻿namespace MultiDialogsBot.Dialogs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.FormFlow;
+    using Microsoft.Bot.Connector;
+
+    [Serializable]
+    public class FindProductDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Welcome, Lets help you find what youre looking for. Please type in your query");
+
+           
+        }
+
+     
+
+    
+       }
+}
---
+++ ap-bot-cs/Dialogs/RootDialog.cs
@@ -0,0 +1,77 @@
+﻿namespace MultiDialogsBot.Dialogs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        private const string SelectShadeOption = "Select Shade";
+
+        private const string FindProductOption = "Find Product";
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            this.ShowOptions(context);
+          
+        }
+
+        private void ShowOptions(IDialogContext context)
+        {
+            PromptDialog.Choice(context, this.OnOptionSelected, new List<string>() { SelectShadeOption, FindProductOption }, "Hi What would you like to do Today?", "Not a valid option", 3);
+        }
+
+        private async Task OnOptionSelected(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                string optionSelected = await result;
+
+                switch (optionSelected)
+                {
+                    case SelectShadeOption:
+                        context.Call(new SelectShadeDialog(), this.ResumeAfterOptionDialog);
+                        break;
+
+                    case FindProductOption:
+                        context.Call(new FindProductDialog(), this.ResumeAfterOptionDialog);
+                        break;
+                }
+            }
+            catch (TooManyAttemptsException ex)
+            {
+                await context.PostAsync($"Ooops! Too many attemps :(. But don't worry, I'm handling that exception and you can try again!");
+
+                context.Wait(this.MessageReceivedAsync);
+            }
+        }
+
+      
+
+        private async Task ResumeAfterOptionDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            try
+            {
+                var message = await result;
+            }
+            catch (Exception ex)
+            {
+                await context.PostAsync($"Failed with message: {ex.Message}");
+            }
+            finally
+            {
+                context.Wait(this.MessageReceivedAsync);
+            }
+        }
+    }
+}
